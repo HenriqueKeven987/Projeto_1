904844f67bf529c60ec49c8c8a4e98d90118aa77
HenriqueKeven987 <henrique.keven@seduc.ce.gov.br>
2020-12-09 16:52:51 -0300

    listar depoimento 0.6

--- painel/pages/listar-depoimentos.php
+++ painel/pages/listar-depoimentos.php
@@ -6,8 +6,17 @@
 
 	<table>
 		<tr>
-			<td>Nome:</td>
-			<td>Depoimentos:</td>
+			<td>Nome</td>
+			<td>Depoimentos</td>
+			<td>#</td>
+			<td>#</td>
+		</tr>
+		<tr>
+			<!--conteudo-->
+			<td>Henrique</td>
+			<td>Meu depoimento</td>
+			<td><a class="btn edit" href=""><i class="fa fa-pencil-alt"></i> Editar</a></td>
+			<td><a class="btn delete" href=""><i class="fa fa-times"></i> Deletar</a></td>
 		</tr>
 	</table>
 
